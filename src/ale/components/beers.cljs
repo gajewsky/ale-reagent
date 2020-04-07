b9650f4fe96e98f4e7a6ba02c366cb2f815410f6
Marcin Gajewski <code@gajewsky.it>
2020-04-07 16:10:01 +0200

    Beers component

--- src/ale/components/beers.cljs
+++ src/ale/components/beers.cljs
@@ -1,5 +1,20 @@
-(ns ale.components.beers)
+(ns ale.components.beers
+  (:require [ale.state :as state]))
+
+(defn beer-component
+  [beer]
+  [:div.beer {:key (:id beer)}
+   [:img.beer__artwork {:src (:img beer) :alt (:name beer)}]
+   [:div.beer__body
+    [:div.beer__name
+     [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to cart"}]
+     [:i.icon.icon--plus] (:name beer)]
+    [:p.beer__price (:price beer)]
+    [:p.beer_desc (:desc beer)]]])
 
 (defn beers
   []
-  [:beers "beers"])
+  [:main
+   [:div.beers (map (fn [beer] (beer-component beer)) (vals @state/beers))]])
+
+
