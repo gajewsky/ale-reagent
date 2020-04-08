476745efecf8bfd3b07089f343af3468ba422865
Marcin Gajewski <code@gajewsky.it>
2020-04-08 16:35:20 +0200

    Calculate total and add remove all button

--- src/ale/components/orders.cljs
+++ src/ale/components/orders.cljs
@@ -5,6 +5,16 @@
   [id]
   (swap! state/orders dissoc id))
 
+(defn remove-all
+  []
+  (reset! state/orders {}))
+
+(defn total
+  []
+  (->> @state/orders
+       (map (fn[[id quantity]] (* quantity (get-in @state/beers [id :price]))))
+       (reduce +)))
+
 (defn order-component
   [id quantity]
   (let [{:keys [img name price]} (get @state/beers id)]
@@ -26,4 +36,15 @@
    [:div.order
     [:div.body
      (for [[id quantity] @state/orders]
-       (order-component id quantity))]]])
+       (order-component id quantity))]]
+
+   [:div.total
+    [:hr]
+    [:div.item
+     [:div.content "Total"]
+     [:div.action
+      [:div.price (total)]]
+     [:button.btn.btn--link.tooltip
+      {:data-tooltip "Remove all"
+       :on-click #(remove-all)}
+      [:i.icon.icon--delete]]]]])
