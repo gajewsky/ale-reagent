7b2c0ffec7a085c861969c1de33d332e495c6f91
Marcin Gajewski <code@gajewsky.it>
2020-04-09 16:50:05 +0200

    Save beer

--- src/ale/components/beer_editor.cljs
+++ src/ale/components/beer_editor.cljs
@@ -2,48 +2,51 @@
 
 (defn form-group
   [{:keys [id type value values]}]
-  [:div.form__group
-   [:label.form__label {:for id} id]
-   [:input.form__input {:type type
-                        :id id
-                        :value value
-                        :on-change #(swap! values assoc (keyword id) (.. % -target -value))}]])
+  (let [update-form #(swap! values assoc (keyword id) (.. % -target -value))]
+    [:div.form__group
+     [:label.form__label {:for id} id]
+     [:input.form__input {:type type
+                          :id id
+                          :value value
+                          :on-change update-form }]]))
 
 (defn beer-editor
-  [modal values]
-  (let [{:keys [title desc img price sold-out]} @values]
+  [{:keys [modal values save-beer toggle-modal initial-values]}]
 
-  [:div.modal (when @modal {:class "active"})
-   [:div.modal__overlay]
-   [:div.modal__container
-    [:div.modal__body
-     [form-group {:id "title"
-                  :type "text"
-                  :value  title
-                  :values values}]
-     [form-group {:id "desc"
-                  :type "text"
-                  :value desc
-                  :values values}]
-     [form-group {:id "img"
-                  :type "text"
-                  :value img
-                  :values values}]
-     [form-group {:id "price"
-                  :type "number"
-                  :value price
-                  :values values}]
-     [:div.form__group
-      [:label.form__label {:for "sold-out"} "sold-out"]
-      [:label.form__switch
-       [:input {:type :checkbox
-                :checked sold-out
-                :on-change #(swap! values assoc :sold-out (.. % -target -checked))}]
-       [:i.form__icon]]]]
-    [:div.modal__footer
-     [:button.btn.btn--link.float--left
-      {:on-click #(reset! modal false)}
-      "Cancel"]
-     [:button.btn.btn--secondary
-      "Save"]]]]))
+  (let [{:keys [name desc img price sold-out]} @values]
+
+    [:div.modal (when (:active @modal) {:class "active"})
+     [:div.modal__overlay]
+     [:div.modal__container
+      [:div.modal__body
+       [form-group {:id "name"
+                    :type "text"
+                    :value  name
+                    :values values}]
+       [form-group {:id "desc"
+                    :type "text"
+                    :value desc
+                    :values values}]
+       [form-group {:id "img"
+                    :type "text"
+                    :value img
+                    :values values}]
+       [form-group {:id "price"
+                    :type "number"
+                    :value price
+                    :values values}]
+       [:div.form__group
+        [:label.form__label {:for "sold-out"} "sold-out"]
+        [:label.form__switch
+         [:input {:type :checkbox
+                  :checked sold-out
+                  :on-change #(swap! values assoc :sold-out (.. % -target -checked))}]
+         [:i.form__icon]]]]
+      [:div.modal__footer
+       [:button.btn.btn--link.float--left
+        {:on-click #(toggle-modal {:active false :beer initial-values})}
+        "Cancel"]
+       [:button.btn.btn--secondary
+        {:on-click #(save-beer @values)}
+        "Save"]]]]))
 
--- src/ale/components/beers.cljs
+++ src/ale/components/beers.cljs
@@ -1,16 +1,20 @@
 (ns ale.components.beers
   (:require [ale.state :as state]
             [ale.helpers :refer [format-price]]
+            [ale.components.beer-editor :refer [beer-editor]]
             [reagent.core :as r]
-            [ale.components.beer-editor :refer [beer-editor]]))
+            [clojure.string :as str]))
 
 (defn beer-component
-  [beer]
+  [beer toggle-modal]
   (let [{:keys [id img desc price name]} beer
         add-to-cart #(swap! state/orders update (:id beer) inc)]
 
     [:div.beer {:key id}
-     [:img.beer__artwork {:src img :alt name}]
+     [:img.beer__artwork.beer__edit {:src img
+                                     :alt name
+                                     :on-click #(toggle-modal {:active true
+                                                               :beer beer})}]
      [:div.beer__body
       [:div.beer__name
        [:div.btn.btn--primary.float--right.tooltip
@@ -22,25 +26,45 @@
 
 (defn beers
   []
-  (let [modal (r/atom false)
-        values (r/atom
-                 {:id nil
-                  :title ""
-                  :desc ""
-                  :price 0
-                  :img ""
-                  :sold-out false})]
+  (let [modal (r/atom {:active false})
+        initial-values {:id nil
+                        :name ""
+                        :desc ""
+                        :price 0
+                        :img ""
+                        :sold-out false}
+        values (r/atom initial-values)
+
+        toggle-modal (fn
+                       [{:keys [active beer]}]
+                       (swap! modal assoc :active active)
+                       (reset! values beer))
+
+        save-beer (fn [{:keys [id name desc price img sold-out]}]
+                    (swap! state/beers assoc id {:id (or id (str "beer-" (random-uuid)))
+                                                 :name (str/trim name)
+                                                 :desc (str/trim desc)
+                                                 :img (str/trim img)
+                                                 :price (js/parseInt price)
+                                                 :sold-out sold-out})
+
+                    (toggle-modal {:active false :beer initial-values}))]
     (fn
       []
       [:main
-       [:div.gigs
-        [:button.add-gig
-         {:on-click #(reset! modal true)}
-         [:div.add__title
+       [:div.beers
+        [:button.add-beer
+         {:on-click #(toggle-modal {:active true :beer initial-values})}
+         [:div.add__name
           [:i.icon.icon--plus]
-          [:p "Add gig"]]]
-        [beer-editor modal values]
+          [:p "Add beer"]]]
+        [beer-editor
+         {:modal modal
+          :values values
+          :save-beer save-beer
+          :toggle-modal toggle-modal
+          :initial-values initial-values}]
         (for [beer (vals @state/beers)]
-          [beer-component beer])]])))
+          [beer-component beer toggle-modal])]])))
 
 
